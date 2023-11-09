c7152c234963606c62a3aae9bdd9c3575e2f70dc
Lucas de Ataides <lucas.deataidesbarreto@windriver.com>
2023-11-09 20:40:43 -0300

    Uppercasing the word 'API'

--- src/main/kotlin/com/ldatb/learn/banking/BankingApiApplication.kt
+++ src/main/kotlin/com/ldatb/learn/banking/BankingApiApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BankingApiApplication
+class BankingAPIApplication
 
 fun main(args: Array<String>) {
-    runApplication<BankingApiApplication>(*args)
+    runApplication<BankingAPIApplication>(*args)
 }
