a092dd9fb5bb0e4e7305b292b31ddf1d8b6df1d5
Lucas de Ataides <lucas.deataidesbarreto@windriver.com>
2023-11-10 14:46:29 -0300

    Unused import

--- src/main/kotlin/com/ldatb/learn/banking/repository/AccountRepository.kt
+++ src/main/kotlin/com/ldatb/learn/banking/repository/AccountRepository.kt
@@ -1,7 +1,6 @@
 package com.ldatb.learn.banking.repository
 
 import com.ldatb.learn.banking.model.Account
-import org.springframework.data.domain.Sort
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
