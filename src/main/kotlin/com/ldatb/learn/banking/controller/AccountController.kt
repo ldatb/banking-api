11a7e7f3f7b765008def95acc58fb6ede2c5711d
Lucas de Ataides <lucas.deataidesbarreto@windriver.com>
2023-11-14 20:09:55 -0300

    Creating basic controllers

--- src/main/kotlin/com/ldatb/learn/banking/controller/AccountController.kt
+++ src/main/kotlin/com/ldatb/learn/banking/controller/AccountController.kt
@@ -1,34 +1,49 @@
 package com.ldatb.learn.banking.controller
 
 import com.ldatb.learn.banking.model.Account
-import com.ldatb.learn.banking.request.CreateAccountRequest
 import com.ldatb.learn.banking.service.AccountService
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("account")
 class AccountController(private val accountService: AccountService) {
+    // GET
     @GetMapping
-    fun getAccounts(): ResponseEntity<List<Account>> =
-        ResponseEntity.ok(accountService.getAllAccounts())
+    fun getAccountByNumber(): ResponseEntity<Account> {
+        TODO()
+    }
 
-    @GetMapping("{id}")
-    fun getAccountByID(@PathVariable id: Long): ResponseEntity<Account> =
-        ResponseEntity.ok(accountService.getAccountByID(id))
+    // LOGIN AND LOGOUT
+    @PostMapping("login")
+    fun loginToAccount(): ResponseEntity<Account> {
+        TODO()
+    }
 
+    @DeleteMapping("login")
+    fun logoutOfAccount(): ResponseEntity<Account> {
+        TODO()
+    }
+
+    // UPDATE
+    @PutMapping("token")
+    fun updateAccountSecretToken(): ResponseEntity<Account> {
+        TODO()
+    }
+
+    // CREATE AND DELETE
     @PostMapping
-    fun createAccount(
-        @RequestBody
-        createAccountRequest: CreateAccountRequest
-    ): ResponseEntity<Account> {
-        return ResponseEntity.status(HttpStatus.CREATED)
-            .body(accountService.createAccount(createAccountRequest))
+    fun createAccount(): ResponseEntity<Account> {
+        TODO()
+    }
+
+    @DeleteMapping
+    fun deleteAccount(): ResponseEntity<Account> {
+        TODO()
     }
 }
---
+++ src/main/kotlin/com/ldatb/learn/banking/controller/TransactionController.kt
@@ -0,0 +1,25 @@
+package com.ldatb.learn.banking.controller
+
+import com.ldatb.learn.banking.model.Transaction
+import com.ldatb.learn.banking.service.TransactionService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("transaction")
+class TransactionController(private val transactionService: TransactionService) {
+    // GET ACCOUNT'S TRANSACTIONS
+    @GetMapping
+    fun getAccountTransactions(): ResponseEntity<List<Transaction>> {
+        TODO()
+    }
+
+    // CREATE NEW TRANSACTION
+    @PostMapping
+    fun createNewTransaction(): ResponseEntity<Transaction> {
+        TODO()
+    }
+}
