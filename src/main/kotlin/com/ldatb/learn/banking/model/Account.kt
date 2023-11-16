6f8ba0e02e59681f84399568059453d266499502
Lucas de Ataides <lucas.deataidesbarreto@windriver.com>
2023-11-16 15:13:02 -0300

    Changin var to val

--- src/main/kotlin/com/ldatb/learn/banking/model/Account.kt
+++ src/main/kotlin/com/ldatb/learn/banking/model/Account.kt
@@ -1,5 +1,6 @@
 package com.ldatb.learn.banking.model
 
+import com.ldatb.learn.banking.dto.response.CreateAccountDetailsDTO
 import com.ldatb.learn.banking.util.generateRandomSecretToken
 import jakarta.persistence.*
 import org.springframework.security.core.GrantedAuthority
@@ -13,44 +14,55 @@ import java.time.Instant
 data class Account(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    var id: Long,
+    val id: Long = 0L,
 
     @Column(name = "login")
-    var login: String,
+    val login: String,
 
     @Column(name = "hashed_password", nullable = false)
-    var hashedPassword: String,
+    val hashedPassword: String,
 
     @Column(name = "transfer_key", length = 36, nullable = false)
     @GeneratedValue(strategy = GenerationType.UUID)
-    var transferKey: String,
+    val transferKey: String = "",
 
     @Column(name = "secret_token", nullable = false)
-    var secretToken: UInt = generateRandomSecretToken(),
+    val secretToken: UInt = generateRandomSecretToken(),
 
     @Column(name = "first_name", nullable = false)
-    var firstName: String,
+    val firstName: String,
 
     @Column(name = "last_name", nullable = false)
-    var lastName: String,
+    val lastName: String,
 
     @Column(name = "balance", nullable = false)
-    var balance: Long = 0L,
+    val balance: Long = 0L,
 
     @Column(name = "default_currency")
-    var defaultCurrency: String = "usd",
+    val defaultCurrency: String = "usd",
 
     @Column(name = "role")
-    var role: AccountRoles = AccountRoles.USER,
+    val role: AccountRoles = AccountRoles.USER,
 
     @Column(name = "updated_at")
-    var updatedAt: Timestamp = Timestamp.from(Instant.now()),
+    val updatedAt: Timestamp = Timestamp.from(Instant.now()),
 
     @Column(name = "created_at")
-    var createdAt: Timestamp = Timestamp.from(Instant.now()),
+    val createdAt: Timestamp = Timestamp.from(Instant.now()),
 ) : UserDetails {
     @ManyToMany(mappedBy = "transactions")
-    var transactions: List<Transaction> = listOf()
+    val transactions: List<Transaction> = listOf()
+
+    fun toCreateAccountDetailsDTO(): CreateAccountDetailsDTO =
+        CreateAccountDetailsDTO(
+            login = this.login,
+            transferKey = this.transferKey,
+            secretToken = this.secretToken,
+            firstName = this.firstName,
+            lastName = this.lastName,
+            balance = this.balance,
+            defaultCurrency = this.defaultCurrency
+        )
 
     override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
         if (this.role == AccountRoles.ADMIN) {
