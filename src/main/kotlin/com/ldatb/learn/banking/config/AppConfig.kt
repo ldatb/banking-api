fea64a9a953171f719747492eb19cc2d4327fe6c
Lucas de Ataides <lucas.deataidesbarreto@windriver.com>
2023-11-16 15:13:34 -0300

    Applying CORS configuration

--- src/main/kotlin/com/ldatb/learn/banking/config/AppConfig.kt
+++ src/main/kotlin/com/ldatb/learn/banking/config/AppConfig.kt
@@ -3,10 +3,25 @@ package com.ldatb.learn.banking.config
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableConfigurationProperties(AppProperties::class)
 class AppConfig {
     @Bean
     fun appConfiguration(): AppProperties = AppProperties()
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        // allow localhost for dev purposes
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("http://localhost:3000", "http://localhost:8080")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
+        configuration.allowedHeaders = listOf("authorization", "content-type")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
 }
